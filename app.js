f940fc38a2534e526b98978f5138534d3ed8c66f
Kevin Sajan <kevsaj@gmail.com>
2021-02-09 23:46:50 -0500

    firing function

--- app.js
+++ app.js
@@ -15,26 +15,29 @@ let ground = Matter.Bodies.rectangle(500, 600, 1400, 30, {
 });
 
 let topWall = Matter.Bodies.rectangle(500, 0, 1400, 20, {
-        isStatic: true, render: {
-            visible: true
-        }
+    isStatic: true,
+    render: {
+        visible: true
+    }
 });
 
 let leftWall = Matter.Bodies.rectangle(0, 250, 20, window.innerHeight, {
-        isStatic: true, render: {
-            visible: true
-        }
+    isStatic: true,
+    render: {
+        visible: true
+    }
 });
 
 let rightWall = Matter.Bodies.rectangle(1200, 250, 20, window.innerHeight, {
-        isStatic: true, render: {
-            visible: true
-        }
+    isStatic: true,
+    render: {
+        visible: true
+    }
 });
 
-let stack = Matter.Composites.stack(400, 270, 6, 6, 0, 0, function(x, y) { 
-          return Matter.Bodies.circle(x, y, 30, 30);
-      });
+let stack = Matter.Composites.stack(400, 270, 10, 10, 0, 0, function (x, y) {
+    return Matter.Bodies.circle(x, y, 30, 30);
+});
 
 let mouse = Matter.Mouse.create(render.canvas);
 let mouseConstraint = Matter.MouseConstraint.create(engine, {
@@ -48,11 +51,27 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
 render.mouse = mouse;
 
 let ball = Matter.Bodies.circle(600, 200, 20);
-let sling = Matter.Constraint.create({ 
-      pointA: { x: 600, y: 200 }, 
-      bodyB: ball, 
-      stiffness: 0.05
-  });
+let sling = Matter.Constraint.create({
+    pointA: {
+        x: 600,
+        y: 200
+    },
+    bodyB: ball,
+    stiffness: 0.05
+});
+
+let firing = false;
+Matter.Events.on(mouseConstraint, 'enddrag', function (e) {
+    if (e.body === ball) firing = true;
+});
+Matter.Events.on(engine, 'afterUpdate', function () {
+    if (firing && Math.abs(ball.position.x - 600) < 20 && Math.abs(ball.position.y - 200) < 20) {
+        ball = Matter.Bodies.circle(600, 200, 20);
+        Matter.World.add(engine.world, ball);
+        sling.bodyB = ball;
+        firing = false;
+    }
+});
 
 
 
